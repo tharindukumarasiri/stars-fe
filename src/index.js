9bdb6367a4b5bf6536059bfa470b4f7ea432c632
Tharindu Kumarasiri <>
2022-07-27 16:53:57 +0530

    NavBar added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
 import 'assets/css/base.scss'
 
 import BuyerRole from "./Views/BuyerRole";
 import SupplireRole from "./Views/SupplierRole"
 
+const NavBar = () => {
+    return (
+        <nav >
+            <NavLink className="navbar-brand" to="/buyer">Buyer</NavLink>
+            <br />
+            <br />
+            <NavLink className="navbar-brand" to="/supplier">Supplier</NavLink>
+        </nav>
+    )
+}
+
 function App() {
     return (
         <Routes>
+            <Route path='/' element={<NavBar />} />
             <Route path='/buyer' element={<BuyerRole />} />
             <Route path='/supplier' element={<SupplireRole />} />
         </Routes>
