b0399d5b5613ee7c172bff2751a6c729650e1c34
Tharindu Kumarasiri <>
2022-12-07 16:34:37 +0530

    toggle switch lib change

--- src/common/toggleSwitch.jsx
+++ src/common/toggleSwitch.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import "../assets/css/_toggle-switch.scss"
 import { useTranslation } from 'react-i18next'
 
-const ToggleSwitch = ({ label, value, onChange }) => {
+const ToggleSwitch = ({ label, onChange, checked }) => {
     const { t } = useTranslation();
 
     return (
         <div className="g-row">
             <div className="g-col-3 m-r-10">{t(label)}</div>
             <div className="g-col-5 toggle-switch">
-                <input type="checkbox" className="checkbox" checked={value} onChange={onChange}
+                <input type="checkbox" className="checkbox" onChange={onChange} checked={checked}
                     name={label} id={label} />
                 <label className="label" htmlFor={label}>
                     <span className="inner" />
