e97f1e7ab8499237c5050973d48363af2db581ad
Tharindu Kumarasiri <ku.tharindu@gmail.com>
2025-05-28 20:00:27 +0530

    language handling changes

--- src/assets/languages/index.js
+++ src/assets/languages/index.js
@@ -1,49 +1,28 @@
 import enTranslation from "./en-US.json";
-import noTranslation from "./nb-NO.json";
-import daTranslation from "./da-DK.json";
-import fiTranslation from "./fi-FI.json";
-import deTranslation from "./de.json";
-import svTranslation from "./sv-SE.json";
-import cnTranslation from "./zh.json";
-import enGBTranslation from "./en-GB.json";
 import { getLanguageFile } from "../../services/applicationService";
 
 const resources = {
   'en-US': {
-     translation: enTranslation 
-    },
-  'nb-NO': {
-    translation: noTranslation
+    translation: enTranslation
   },
-  'da-DK': {
-    translation: daTranslation
-  },
-  'fi-FI': {
-    translation: fiTranslation
-  },
-  'de': {
-    translation: deTranslation
-  },
-  'sv-SE': {
-    translation: svTranslation
-  },
-  'zh': {
-    translation: cnTranslation
-  },
-  'en-GB': {
-    translation: enGBTranslation
-  }
+}
+
+function pascalToSnakeCase(word) {
+  return word
+    .replace(/([a-z0-9])([A-Z])/g, '$1_$2') // Add underscore between camel bumps
+    .replace(/([A-Z])([0-9])/g, '$1_$2')     // Add underscore before numbers
+    .toUpperCase().trim();
 }
 
 const transformArrayToObject = async (arr) => {
-  try{
+  try {
     const result = {};
 
     arr.forEach(item => {
       const finalValue = item.Value && item.Value.trim() !== "" ? item.Value : item.DefaultValue;
-      result[item.Key] = finalValue;
+      result[pascalToSnakeCase(item.Key)] = finalValue;
     });
-  
+
     return result;
   } catch {
     console.log('Language file convert failed')
@@ -51,20 +30,21 @@ const transformArrayToObject = async (arr) => {
 }
 
 const fetchLanguage = async () => {
-  try{
+  try {
     const cultureCode = localStorage.getItem('i18nextLng');
 
     const response = await getLanguageFile(cultureCode)
     const data = await response.data
     const languageObj = await transformArrayToObject(data);
-  
+
     return {
       [cultureCode]: {
-         translation: languageObj 
-        }
+        translation: languageObj
+      }
     };
   } catch {
     console.log('Language file fetch failed')
+    return resources;
   }
 }
 
